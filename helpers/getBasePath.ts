46a82613f1b419d51bfa7a8cb3ad9dde7a6ab8e0
magnifiques <arpit.vaghela@icloud.com>
2023-05-05 00:13:14 +0530

    changes in public url

--- helpers/getBasePath.ts
+++ helpers/getBasePath.ts
@@ -1,8 +1,8 @@
 const getBasePath = () => {
-  let BASE_URL =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : `${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  let BASE_URL = `${process.env.NEXT_PUBLIC_PUBLIC_URL}`;
+  // process.env.NODE_ENV === "development"
+  //   ? "http://localhost:3000"
+  //   : `https://howstheweattherupthere.vercel.app`;
 
   return BASE_URL;
 };
